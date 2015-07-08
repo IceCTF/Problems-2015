17f3557298c7737057396704a27a0b16f8a3d31b
James Elías <jamiees2@gmail.com>
2015-07-08 10:15:28 +0000

    updated liar taunt

--- stage1/web/liar/grader/grader.py
+++ stage1/web/liar/grader/grader.py
@@ -3,6 +3,6 @@ def grade(autogen, key):
     if "hidden_in_the_code" in key.lower():
         return True, "Correct!"
     elif "not_this_time" in key.lower():
-        return False, "Not this time!"
+        return False, "Not this time! You didn't think it would be that easy, right?"
     else:
         return False, "Try Again."
