d167d1fa77ae8c954ddb49e6148d5806a5d21cda
James Elías <jamiees2@gmail.com>
2015-07-22 23:36:06 +0100

    fixed things

--- stage1/crypto/rot13/grader/grader.py
+++ stage1/crypto/rot13/grader/grader.py
@@ -1,5 +1,5 @@
 def grade(arg, key):
-    if "rot13_isnt_secure" in key:
+    if "rot_13_isnt_secure" in key:
         return True, "Yes, exactly! Now tell that to Jeff."
     else:
         return False, "No dice."
