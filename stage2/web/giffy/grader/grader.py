533894c1bae99f1b17aab4af4f46d3b49e45bd22
James Elías <jamiees2@gmail.com>
2015-07-25 21:46:40 +0100

    thing

--- stage2/web/giffy/grader/grader.py
+++ stage2/web/giffy/grader/grader.py
@@ -1,6 +1,6 @@
 
 def grade(autogen, key):
-    if "flag_donald_62Ia6KTXSVviM" in key.lower():
+    if "flag_donald_62Ia6KTXSVviM" in key:
         return True, "Correct! The gif is here btw: http://giphy.com/gifs/disney-haters-donald-62Ia6KTXSVviM"
     else:
         return False, "Try Again."
