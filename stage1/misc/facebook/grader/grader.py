ba217d364106812d8aae976136b881a4680e4492
James Elías <jamiees2@gmail.com>
2015-08-09 13:00:06 +0000

    updated facebook problem

--- stage1/misc/facebook/grader/grader.py
+++ stage1/misc/facebook/grader/grader.py
@@ -1,5 +1,5 @@
 def grade(arg, key):
-    if "flag_ba7da3b3439b28b39eaa9" in key:
+    if "flag_why_hide_stuff_on_facebook" in key:
         return True, "Correct!"
     else:
         return False, "Nope."
